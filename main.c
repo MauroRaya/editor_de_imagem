5fc52d8de67af54cce299fdd1cef17eeb8e3272c
MauroRaya <maurorayabr@gmail.com>
2024-08-21 16:49:55 -0300

    adicionando bibliotecas

--- main.c
+++ main.c
@@ -1,56 +1,30 @@
-#include <stdlib.h>
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "stb_image.h"
+#include "stb_image_write.h"
 #include <stdio.h>
-#include <dirent.h>
-#include <sys/stat.h>
+#include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 
+void aplicarEfeito(const char *efeito, const char *caminho, const char *destino);
 
-#pragma pack(1)
-typedef struct {
-    uint16_t bfType;
-    uint32_t bfSize;
-    uint16_t bfReserved1;
-    uint16_t bfReserved2;
-    uint32_t bfOffBits;
-} BITMAPFILEHEADER;
-
-typedef struct {
-    uint32_t biSize;
-    int32_t biWidth;
-    int32_t biHeight;
-    uint16_t biPlanes;
-    uint16_t biBitCount;
-    uint32_t biCompression;
-    uint32_t biSizeImage;
-    int32_t biXPelsPerMeter;
-    int32_t biYPelsPerMeter;
-    uint32_t biClrUsed;
-    uint32_t biClrImportant;
-} BITMAPINFOHEADER;
-
-
-int aplicarGrayscale();
-int aplicarInvert();
-
-
-int main (int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        printf("Uso: %s [grayscale|invert]\n");
+        printf("Uso: %s [grayscale|invert]\n", argv[0]);
         return 1;
     }
 
     DIR *pasta;
     struct dirent *entrada;
-    struct stat info;
 
-    pasta = opendir("../imagens");
-
+    pasta = opendir("../imagens/");
     if (pasta == NULL)
     {
-        puts("Não foi possivel encontrar o diretório");
-        return 1;
+        printf("Não foi possivel abrir pasta de imagens\n");
     }
 
     while ((entrada = readdir(pasta)) != NULL)
@@ -61,28 +35,70 @@ int main (int argc, char *argv[])
         }
 
         char caminho[256];
+        snprintf(caminho, sizeof(caminho), "../imagens/%s", entrada->d_name);
 
-        strcpy(caminho, "../imagens/");
-        strcat(caminho, entrada->d_name);
-
-        if (stat(caminho, &info) == 0)
+        char destino[256];
+        if (strcmp(argv[1], "grayscale") == 0)
         {
-            if (strcmp(argv[1], "grayscale") == 0)
-            {
+            snprintf(destino, sizeof(destino), "../imagens_editadas/grayscale/%s", entrada->d_name);
+        }
+        else if (strcmp(argv[1], "invert") == 0)
+        {
+            snprintf(destino, sizeof(destino), "../imagens_editadas/invert/%s", entrada->d_name);
+        }
+        else
+        {
+            printf("Efeito desconhecido: %s\n", argv[1]);
+            closedir(pasta);
+            return 1;
+        }
 
-            }
-            else if (strcmp(argv[1], "invert") == 0)
-            {
+        aplicarEfeito(argv[1], caminho, destino);
+    }
 
-            }
-            else
-            {
-                printf("Opção desconhecida: %s\n", argv[1]);
-                return 1;
-            }
+    return 0;
+}
+
+void aplicarEfeito(const char *efeito, const char *caminho, const char *destino)
+{
+    int largura, altura, canais;
+    unsigned char *imagem = stbi_load(caminho, &largura, &altura, &canais, 0);
+
+    if (imagem == NULL)
+    {
+        printf("Erro ao carregar a imagem %s\n", caminho);
+        return;
+    }
+
+    printf("Processando imagem: %s\n", caminho);
+
+    for (int i = 0; i < largura * altura * canais; i += canais)
+    {
+        unsigned char r = imagem[i];
+        unsigned char g = imagem[i + 1];
+        unsigned char b = imagem[i + 2];
+
+        if (strcmp(efeito, "grayscale") == 0)
+        {
+            unsigned char gray = (r + g + b) / 3;
+            imagem[i] = imagem[i + 1] = imagem[i + 2] = gray;
+        }
+        else if (strcmp(efeito, "invert") == 0)
+        {
+            imagem[i] = 255 - r;
+            imagem[i + 1] = 255 - g;
+            imagem[i + 2] = 255 - b;
         }
     }
 
-    closedir(pasta);
-    return 0;
+    if (stbi_write_jpg(destino, largura, altura, canais, imagem, 100))
+    {
+        printf("Arquivo editado com sucesso: %s\n", destino);
+    }
+    else
+    {
+        printf("Erro ao salvar a imagem %s\n", destino);
+    }
+
+    stbi_image_free(imagem);
 }
